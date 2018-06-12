471f4998db4f187155eda0cef407ca36019dbdbf
Nicolas Garnier <nivco@google.com>
2018-06-12 14:42:08 +0200

    Added Service workers using Workbox to enable offline of static assets.

---
+++ public/sw.js
@@ -0,0 +1,60 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+
+if (workbox) {
+  workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.networkFirst()
+  );
+
+  workbox.routing.registerRoute(
+    // Cache CSS files
+    /.*\.css/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+      // Use a custom cache name
+      cacheName: 'css-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    // Cache HTML files
+  /.*\.html/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+      // Use a custom cache name
+      cacheName: 'html-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    // Cache HTML files
+  /[^\.]*/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+      // Use a custom cache name
+      cacheName: 'html-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    // Cache image files
+    /.*\.(?:png|jpg|jpeg|svg|gif)/,
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache only 20 images
+          maxEntries: 20,
+          // Cache for a maximum of a week
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        }),
+      ],
+    })
+  );
+
+  workbox.googleAnalytics.initialize();
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
