428bafc17fd62e0dc60973f93c6920337a7ad8a1
Nicolas Garnier <nivco@google.com>
2018-06-14 15:17:07 +0200

    injecting filnmaes in index.html and generating Firebase messaging SW file.

--- public/firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-// Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/5.0.4/firebase-app.js');
-importScripts('/__/firebase/5.0.4/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
-
-firebase.messaging();
--- webpack.config.js
+++ webpack.config.js
@@ -1,110 +1,147 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
 const path = require('path');
 const glob = require('glob-all');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const PurifyCSSPlugin = require('purifycss-webpack');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'production', // "production" | "development" | "none"  // Chosen mode tells webpack to use its built-in optimizations accordingly.
-  entry: './src/app.js', // string | object | array  // Here the application starts executing
-  // and webpack starts bundling
-  output: {
-    // options related to how webpack emits results
-    path: path.resolve(__dirname, 'public'), // string
-    // the target directory for all output files
-    // must be an absolute path (use the Node.js path module)
-    filename: 'bundle.js', // string    // the filename template for entry chunks
-  },
-  performance: {
-    hints: 'warning', // enum    maxAssetSize: 200000, // int (in bytes),
-    maxEntrypointSize: 400000, // int (in bytes)
-    assetFilter: function(assetFilename) {
-      // Function predicate that provides asset filenames
-      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== 'production';
+
+  return {
+    entry: {
+      app: './src/app.js',
+      sw: './src/firebase-messaging-sw.js',
     },
-  },
-  devtool: 'source-map', // enum  // enhance debugging by adding meta info for the browser devtools
-  // source-map most detailed at the expense of build speed.
-  context: __dirname, // string (absolute path!)
-  // the home directory for webpack
-  // the entry and module.rules.loader option
-  //   is resolved relative to this directory
-  target: 'web', // enum  // the environment in which the bundle should run
-  // changes chunk loading behavior and available modules
-  externals: ['react'], // Don't follow/bundle these modules, but request them at runtime from the environment
-  serve: { // object
-    port: 5000,
-    content: './public',
-    // ...
-  },
-  // lets you provide options for webpack-serve
-  stats: 'errors-only', // lets you precisely control what bundle information gets displayed
-  devServer: {
-    proxy: { // proxy URLs to backend development server
-      '/api': 'http://localhost:3000',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: (entrypoint) => {
+        // console.log('XXX', a);
+        if (entrypoint.chunk.name === 'app') {
+          return 'js/bundle.[hash].js';
+        } else {
+          return 'firebase-messaging-sw.js';
+        }
+      },
     },
-    contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
-    compress: true, // enable gzip compression
-    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
-    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-    https: false, // true for self-signed, object for cert authority
-    noInfo: true, // only errors & warns on hot reload
-    // ...
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-        }),
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]',
+    devtool: devMode ? 'cheap-module-source-map' : 'source-map',
+    context: __dirname,
+    target: 'web',
+    stats: 'errors-only', // lets you precisely control what bundle information gets displayed
+    devServer: {
+      contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
+      compress: true, // enable gzip compression
+      historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+      inline: true,
+      port: 8080,
+      hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+      https: false, // true for self-signed, object for cert authority
+      noInfo: true, // only errors & warns on hot reload
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
             },
-          },
-        ],
-      },
-      {
-        test: /firebaseui\.css$/,
-        loader: 'string-replace-loader',
-        include: path.resolve('node_modules/firebaseui/dist/'),
-        query: {
-          search: '@import url(https://fonts.googleapis.com/css?family=Roboto:400,500,700);',
-          replace: '',
+          ],
         },
-      },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            'css-hot-loader',
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'fonts/[name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /firebaseui\.css$/,
+          loader: 'string-replace-loader',
+          include: path.resolve('node_modules/firebaseui/dist/'),
+          query: {
+            search: '@import url(https://fonts.googleapis.com/css?family=Roboto:400,500,700);',
+            replace: '',
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimizer: devMode ? [] : [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true,
+        }),
+        new OptimizeCssAssetsPlugin({
+          assetNameRegExp: /css\/styles\..*\.css$/g,
+          cssProcessor: require('cssnano'),
+          cssProcessorOptions: {discardComments: {removeAll: true}},
+          canPrint: true,
+          sourceMap: true,
+        }),
+        new PurifyCSSPlugin({
+          // Give paths to all assets that generate DOM content.
+          paths: glob.sync([
+            path.join(__dirname, 'src/*.js'),
+            path.join(__dirname, 'node_modules/firebaseui/dist/firebaseui.js'),
+            path.join(__dirname, 'node_modules/material-design-lite/material.js'),
+            path.join(__dirname, 'public/index.html'),
+          ]),
+        }),
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/styles.[hash].css',
+      }),
+      // new HtmlCriticalPlugin({
+      //   base: path.join(path.resolve(__dirname), 'dist/'),
+      //   src: 'index.html',
+      //   dest: 'index.html',
+      //   inline: true,
+      //   minify: true,
+      //   extract: true,
+      //   width: 375,
+      //   height: 565,
+      //   penthouse: {
+      //     blockJSRequests: false,
+      //   },
+      // }),
     ],
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css'),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /styles\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorOptions: {discardComments: {removeAll: true}},
-      canPrint: true,
-    }),
-    new PurifyCSSPlugin({
-      // Give paths to parse for rules. These should be absolute!
-      paths: glob.sync([
-        path.join(__dirname, 'src/*.js'),
-        path.join(__dirname, 'node_modules/firebaseui/dist/firebaseui.js'),
-        path.join(__dirname, 'node_modules/material-design-lite/material.js'),
-        path.join(__dirname, 'public/index.html'),
-      ]),
-    }),
-  ],
+  };
 };
